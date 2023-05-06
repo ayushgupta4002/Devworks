0ca91dfd4f3469143ffc69956c264852f4c1e721
Ayush <ayush4002gupta@gmail.com>
2023-05-07 00:42:29 +0530

    backend completed

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors')
-
+var nodemailer = require('nodemailer');
 const PORT = 5000;
-let Validator = require('validatorjs');
+const validator = require("validator")
 const app = express();
+require("dotenv").config();
 
 
 app.use(express.json());
@@ -17,6 +18,64 @@ app.get("/",(req,res) => {
         message:"welcome to this server"
     });
 });
+
+app.post("/send",(req,res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const requirement = req.body.requirement;
+    try {
+  
+        if (validator.isEmpty(name) || validator.isEmpty(requirement)) {
+          res.send({ message: 'one or more fields are empty' });
+    
+        }
+        if (!validator.isEmail(email)) {
+          res.send({ message: 'Email is not Valid' });
+        }
+         if (validator.isEmail(email) == true && validator.isEmpty(name) == false && validator.isEmpty(requirement) == false) {
+            var transport = nodemailer.createTransport({
+                host: "smtp.gmail.com",
+                port: 587,
+                auth: {
+                    user: process.env.EMAIL_SENDER,
+                    pass: process.env.GMAIL_KEY
+                },
+                tls: {
+                    rejectUnauthorized: false
+                }
+            });
+    
+            var mailOptions = {
+                from: `"DevWorks.in" <${process.env.EMAIL_SENDER}>`,
+                to: `${process.env.EMAIL_RECEIVER1},${process.env.EMAIL_RECEIVER2}`,
+                subject: `you got a request`,
+                text: `Hey there, Welcome to DevFeed.in , Your daily solution to keep you updated`,
+                html: `<b>name:</b> ${name} <p><b>email:</b> ${email} <p><b>requirement:</b> ${requirement}`,
+    
+            };
+    
+            transport.sendMail(mailOptions, (error, info) => {
+                if (error) {
+    
+                    return console.log(error);
+                }
+                console.log(info);
+                res.send({
+                    message:"sent"
+                });
+                console.log('Message sent: %s', info.messageId);
+            });
+ 
+    
+        }
+      } catch (error) {
+        console.log(error);
+    
+      }
+
+
+});
+
 
 
 
