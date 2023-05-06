ccf9a8f40136b3390906f5f7eacdc4bb094fc5b2
Ayush <ayush4002gupta@gmail.com>
2023-05-06 19:14:38 +0530

    first commit

---
+++ client/src/components/Hero/Hero.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { fadeIn, slideIn, staggerContainer } from "../../utils/motion";
+import css from "./Hero.module.scss";
+import { motion } from "framer-motion";
+const Hero = () => {
+  return (
+    <section className={`paddings ${css.wrapper}`}>
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`innerWidth ${css.container}`}
+      >
+        <div className={css.upperElements}>
+          <motion.span className="primaryText" variants={fadeIn("right", "tween", 0.2, 1)}>
+           Discuss. Develop.
+            <br />
+            Deliver.
+          </motion.span>
+          <motion.span className="secondaryText"variants={fadeIn("left", "tween", 0.4, 1)}>
+            We design Best Projects for 
+            <br />
+            you, And we love what we do{" "}
+          </motion.span>
+        </div>
+
+        <motion.div
+          variants={fadeIn("up", "tween", 0.3, 1)}
+          className={css.person}
+        >
+          <motion.img variants={slideIn("up", "tween", 0.5, 1.3)} src="./person.png" alt="" />
+        </motion.div>
+
+        <a className={css.email} href="mailto:zainkeepscode@gmail.com">
+          zainkeepscode@gmail.com
+        </a>
+
+        <div className={css.lowerElements}>
+          <motion.div variants={fadeIn("right", "tween", 0.3, 1)} className={css.experience}>
+            <div className="primaryText">10</div>
+            <div className="secondaryText">
+              <div>Times</div>
+              <div>Cheaper</div>
+            </div>
+          </motion.div>
+
+          <motion.div variants={fadeIn("left", "tween", 0.5, 1)} className={css.certificate}>
+            <img src="./certificate.png" alt="" />
+            <span> PROFESSIONAL</span>
+            <span>Developers</span>
+          </motion.div>
+        </div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ client/src/utils/data.js
@@ -0,0 +1,156 @@
+import { HiOutlineDesktopComputer } from "react-icons/hi";
+import { CiMobile1 } from "react-icons/ci";
+import { MdWorkspacesOutline } from "react-icons/md";
+export const projectExperience = [
+  {
+    name: "Quality",
+    projects: " Committed to delivering high-quality and optimizaed projects ",
+    icon: HiOutlineDesktopComputer,
+    bg: "#286F6C",
+  },
+  {
+    name: "Customer Service",
+    projects: "We are always available to answer any questions or concerns.",
+    icon: CiMobile1,
+    bg: "#EEC048",
+  },
+  {
+    name: "Competitive Pricing",
+    projects: "Competitive development services, without compromising on quality.",
+    icon: MdWorkspacesOutline,
+    bg: "#F26440",
+  },
+];
+
+export const WhatDoIHelp = [
+"At DevWorks, we specialize in providing high-quality and optimized development services to organizations of all sizes",
+  "We understand that having a strong online presence is essential for any modern business, and that's why we work closely with our clients to ensure that their websites are visually appealing, user-friendly, and optimized for search engines.",
+];
+
+export const workExp = [
+  {
+    place: "Self-Employed, Brisbane",
+    tenure: "Aug 2014 - Sep 2016",
+    role: "Visual Designer",
+    detail:
+      "A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts",
+  },
+  {
+    place: "New Man Services",
+    tenure: "Aug 2014 - Sep 2016",
+    role: "UI/UX Designer",
+    detail:
+      "A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts",
+  },
+  {
+    place: "Global Solution",
+    tenure: "Aug 2014 - Sep 2016",
+    role: "Sr. Product Designer",
+    detail:
+      "A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts",
+  },
+];
+
+export const comments = [
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+];
+
+export const sliderSettings = {
+  dots: true,
+  infinite: false,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  initialSlide: 0,
+  touchMove: true,
+  useCSS: true,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
